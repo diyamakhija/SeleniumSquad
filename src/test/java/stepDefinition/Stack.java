18b5a6726ee7fcfefc6378ca27ee80edf5a559b3
annapurna-2025 <nayak.annapurna26@gmail.com>
2025-02-18 17:06:16 -0800

    Rectfied the missing and misplaced files

---
+++ src/test/java/stepDefinition/Stack.java
@@ -0,0 +1,28 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stack {
+	@Given("The user is on the Home page after logging in")
+	public void the_user_is_on_the_home_page_after_logging_in() {
+	    
+	}
+
+	@When("The user clicks the {string} button of the {string} in the Data Structure page")
+	public void the_user_clicks_the_button_of_the_in_the_data_structure_page(String string, String string2) {
+	    
+	}
+
+	@Then("The user should be navigated to the {string} page")
+	public void the_user_should_be_navigated_to_the_page(String string) {
+	    
+	}
+
+	@Given("The user is in the {string} page")
+	public void the_user_is_in_the_page(String string) {
+	    
+	}
+
+}
