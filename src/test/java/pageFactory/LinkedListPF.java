ae009bcc6f598bd06d777ed8c75f125065f8d1eb
Sri Hema Saranya Sekar <saranya.s.umashankar@gmail.com>
2025-03-09 18:58:16 -0700

    My LinkedList Module

--- src/test/java/pageFactory/LinkedListPF.java
+++ src/test/java/pageFactory/LinkedListPF.java
@@ -5,55 +5,224 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LinkedListPF {
+import utilities.ExcelSheetReader;
+
+public class LinkedListPF extends BaseClass {
 
     WebDriver driver;
 
-    // Home Page Button (Get Started)
-    @FindBy(xpath = "//button[text()='Get Started']")
-    private WebElement getStartedButton;
+   
 
-    // Drop-down Menu Option for Linked List
-    @FindBy(xpath = "//select[@id='dropdown']//option[text()='Linked List']")
-    private WebElement linkedListDropdownOption;
+    @FindBy(xpath = "//div[@class='col'][3]/div/div/a")
+    private WebElement linkedListGetStartedBtn;
 
-    // Linked List Page elements
-    @FindBy(xpath = "//h1[text()='Linked List']")
-    private WebElement linkedListPageHeader;
+    @FindBy(xpath = "//a[@href='introduction']")
+    private WebElement introductionBtn;
+    
+    @FindBy(xpath = "//a[@href='creating-linked-list']")
+    private WebElement creatingLinkedListBtn;
+    
+    @FindBy(xpath = "//a[@href='types-of-linked-list']")
+    private WebElement typesOfLinkedListBtn;
+    
+    @FindBy(xpath = "//a[@href='implement-linked-list-in-python']")
+    private WebElement implementLinkedListInPythonBtn;
 
-    // Introduction Button
-    @FindBy(xpath = "//button[text()='Introduction']")
-    private WebElement introductionButton;
+    @FindBy(xpath = "//a[@href='traversal']")
+    private WebElement traversalBtn;
+    
+    @FindBy(xpath = "//a[@href='insertion-in-linked-list']")
+    private WebElement insertionBtn;
+    
+    @FindBy(xpath = "//a[@href='deletion-in-linked-list']")
+    private WebElement deletionBtn;
+    
+    @FindBy(xpath = "//a[text()='Try here>>>']")
+	public WebElement tryHereBtn;
 
-    // Try Here Button
-    @FindBy(xpath = "//button[text()='Try Here']")
-    private WebElement tryHereButton;
+	@FindBy(xpath = "//textarea[@tabindex='0']")
+	public WebElement input;
+	
+	@FindBy(xpath = "//pre[@id='output']")
+	public WebElement output;
 
-    // Constructor
-    public LinkedListPF(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(driver, this);
+	@FindBy(xpath = "//button[text()='Run']")
+	public WebElement runBtn;
+	
+	@FindBy(xpath = "//a[text()='Practice Questions']")
+	public WebElement practiceQuestionsBtn;
+	
+	@FindBy(xpath = "//div[@class='nav-item dropdown']/a")
+	public WebElement dropDownBtn;
+
+	@FindBy(xpath = "//div[@class='nav-item dropdown show']/div/a[5]")
+	public WebElement treeOption;
+
+	@FindBy(xpath = "//div[@class='bs-example']/nav/a")
+	public WebElement numpyNinjaBtn;
+	
+
+    public LinkedListPF() {
+		super();
     }
 
-    // Actions on the page
-
-    public void clickGetStartedButton() {
-        getStartedButton.click();
+    public void linkedListGetStartedBtn() {
+    	linkedListGetStartedBtn.click();
     }
 
-    public void selectLinkedListFromDropdown() {
-        linkedListDropdownOption.click();
+    public void introductionBtn() {
+    	introductionBtn.click();
+    }
+    
+    public void creatingLinkedListBtn() {
+    	creatingLinkedListBtn.click();
+    }
+    
+    public void typesOfLinkedListBtn() {
+    	typesOfLinkedListBtn.click();
+    }
+    
+    public void implementLinkedListInPythonBtn() {
+    	implementLinkedListInPythonBtn.click();
+    }
+    
+    public void traversalBtn() {
+    	traversalBtn.click();
+    }
+    
+    public void insertionBtn() {
+    	insertionBtn.click();
+    }
+    
+    public void DeletionBtn() {
+    	deletionBtn.click();
     }
 
-    public boolean isLinkedListPageDisplayed() {
-        return linkedListPageHeader.isDisplayed();
+    public void tryHereBtn() {
+    	tryHereBtn.click();
+        
     }
+    
+    public void input() {
+		input.sendKeys(ExcelSheetReader.pythonCodeData(2).get(0));
+	}
 
-    public void clickIntroductionButton() {
-        introductionButton.click();
-    }
+	public void input1() {
+		input.sendKeys(ExcelSheetReader.pythonCodeData(3).get(0));
+	}
 
-    public void clickTryHereButton() {
-        tryHereButton.click();
+	public void input2() {
+		input.sendKeys(ExcelSheetReader.pythonCodeData(1).get(0));
+	}
+    
+    public void runBtn() {
+    	runBtn.click();
+        
     }
+    
+    public void practiceQuestionsBtn() {
+		practiceQuestionsBtn.click();
+	}
+
+	public void dropDownBtn() {
+		dropDownBtn.click();
+	}
+
+	public void treeOption() {
+		treeOption.click();
+	}
+
+	public void numpyNinjaBtn() {
+		numpyNinjaBtn.click();
+	}
+
+	public String handleAlert1() {
+		return handleAlert();
+
+	}
+
+	public String nameHandleAlert() {
+		return handleAlert();
+
+	}
+
+	public String syntaxHandleAlert() {
+		return handleAlert();
+
+	}
+
+	public String expectedNameErrorResult() {
+		return ExcelSheetReader.pythonCodeData(2).get(1);
+
+	}
+
+	public String expectedSyntaxErrorResult() {
+		return ExcelSheetReader.pythonCodeData(3).get(1);
+
+	}
+	
+	public String expectederrorResult() {
+		return ExcelSheetReader.pythonCodeData(4).get(1);
+
+	}
+
+	public String expectedUrl0() {
+		return ExcelSheetReader.expectedUrl(0).get(0);
+
+	}
+
+	public String expectedUrl3() {
+		return ExcelSheetReader.expectedUrl(3).get(0);
+	}
+
+	public String validOutput() {
+		return ExcelSheetReader.pythonCodeData(1).get(1);
+
+	}
+
+	public String expectedUrl20() {
+		return ExcelSheetReader.expectedUrl(20).get(0);
+
+	}
+
+	public String expectedUrl21() {
+		return ExcelSheetReader.expectedUrl(21).get(0);
+
+	}
+
+	public String expectedUrl22() {
+		return ExcelSheetReader.expectedUrl(22).get(0);
+
+	}
+
+	public String expectedUrl23() {
+		return ExcelSheetReader.expectedUrl(23).get(0);
+
+	}
+
+	public String expectedUrl24() {
+		return ExcelSheetReader.expectedUrl(24).get(0);
+
+	}
+
+	public String expectedUrl25() {
+		return ExcelSheetReader.expectedUrl(25).get(0);
+
+	}
+
+	public String expectedUrl26() {
+		return ExcelSheetReader.expectedUrl(26).get(0);
+
+	}
+	
+	public String expectedUrl27() {
+		return ExcelSheetReader.expectedUrl(27).get(0);
+
+	}
+	
+	public String expectedUrl28() {
+		return ExcelSheetReader.expectedUrl(28).get(0);
+
+	}
+
 }
