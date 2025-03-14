0a2138c047cbb6361e4e6070f1ccea46c0292c0f
Sri Hema Saranya Sekar <saranya.s.umashankar@gmail.com>
2025-03-13 19:37:35 -0700

    changed the xpath of input

--- src/test/java/pageFactory/LinkedListPF.java
+++ src/test/java/pageFactory/LinkedListPF.java
@@ -1,51 +1,60 @@
 package pageFactory;
 
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import utilities.ExcelSheetReader;
 
 public class LinkedListPF extends BaseClass {
 
+	@FindBy(xpath = "//div[@class='col'][3]/div/div/a")
+	private WebElement linkedListGetStartedBtn;
 
-    @FindBy(xpath = "//div[@class='col'][3]/div/div/a")
-    private WebElement linkedListGetStartedBtn;
+	@FindBy(xpath = "//a[@href='introduction']")
+	private WebElement introductionBtn;
 
-    @FindBy(xpath = "//a[@href='introduction']")
-    private WebElement introductionBtn;
-    
-    @FindBy(xpath = "//a[@href='creating-linked-list']")
-    private WebElement creatingLinkedListBtn;
-    
-    @FindBy(xpath = "//a[@href='types-of-linked-list']")
-    private WebElement typesOfLinkedListBtn;
-    
-    @FindBy(xpath = "//a[@href='implement-linked-list-in-python']")
-    private WebElement implementLinkedListInPythonBtn;
+	@FindBy(xpath = "//a[@href='creating-linked-list']")
+	private WebElement creatingLinkedListBtn;
 
-    @FindBy(xpath = "//a[@href='traversal']")
-    private WebElement traversalBtn;
-    
-    @FindBy(xpath = "//a[@href='insertion-in-linked-list']")
-    private WebElement insertionBtn;
-    
-    @FindBy(xpath = "//a[@href='deletion-in-linked-list']")
-    private WebElement deletionBtn;
-    
-    @FindBy(xpath = "//a[text()='Try here>>>']")
+	@FindBy(xpath = "//a[@href='types-of-linked-list']")
+	private WebElement typesOfLinkedListBtn;
+
+	@FindBy(xpath = "//a[@href='implement-linked-list-in-python']")
+	private WebElement implementLinkedListInPythonBtn;
+
+	@FindBy(xpath = "//a[@href='traversal']")
+	private WebElement traversalBtn;
+
+	@FindBy(xpath = "//a[@href='insertion-in-linked-list']")
+	private WebElement insertionBtn;
+
+	@FindBy(xpath = "//a[@href='deletion-in-linked-list']")
+	private WebElement deletionBtn;
+
+	@FindBy(xpath = "//a[text()='Try here>>>']")
 	public WebElement tryHereBtn;
 
-	@FindBy(xpath = "//textarea[@tabindex='0']")
-	public WebElement input;
-	
+	@FindBy(xpath = "//form[@id='answer_form']")
+	public WebElement form;
+
+	@FindBy(xpath = "//form/div/div/div/textarea")
+	public WebElement editorInput;
+
 	@FindBy(xpath = "//pre[@id='output']")
 	public WebElement output;
 
 	@FindBy(xpath = "//button[text()='Run']")
 	public WebElement runBtn;
-	
+
 	@FindBy(xpath = "//a[text()='Practice Questions']")
 	public WebElement practiceQuestionsBtn;
-	
+
 	@FindBy(xpath = "//div[@class='nav-item dropdown']/a")
 	public WebElement dropDownBtn;
 
@@ -54,63 +63,74 @@ public class LinkedListPF extends BaseClass {
 
 	@FindBy(xpath = "//div[@class='bs-example']/nav/a")
 	public WebElement numpyNinjaBtn;
-	
 
-    public void linkedListGetStartedBtn() {
-    	linkedListGetStartedBtn.click();
-    }
-
-    public void introductionBtn() {
-    	introductionBtn.click();
-    }
-    
-    public void creatingLinkedListBtn() {
-    	creatingLinkedListBtn.click();
-    }
-    
-    public void typesOfLinkedListBtn() {
-    	typesOfLinkedListBtn.click();
-    }
-    
-    public void implementLinkedListInPythonBtn() {
-    	implementLinkedListInPythonBtn.click();
-    }
-    
-    public void traversalBtn() {
-    	traversalBtn.click();
-    }
-    
-    public void insertionBtn() {
-    	insertionBtn.click();
-    }
-    
-    public void DeletionBtn() {
-    	deletionBtn.click();
-    }
-
-    public void tryHereBtn() {
-    	tryHereBtn.click();
-        
-    }
-    
-    public void input() {
-		input.sendKeys(ExcelSheetReader.pythonCodeData(2).get(0));
+	public void linkedListGetStartedBtn() {
+		linkedListGetStartedBtn.click();
 	}
 
-	public void input1() {
-		input.sendKeys(ExcelSheetReader.pythonCodeData(3).get(0));
+	public void introductionBtn() {
+		introductionBtn.click();
 	}
 
-	public void input2() {
-		input.sendKeys(ExcelSheetReader.pythonCodeData(1).get(0));
+	public void creatingLinkedListBtn() {
+		creatingLinkedListBtn.click();
 	}
-    
-    public void runBtn() {
-    	runBtn.click();
-        
-    }
-    
-    public void practiceQuestionsBtn() {
+
+	public void typesOfLinkedListBtn() {
+		typesOfLinkedListBtn.click();
+	}
+
+	public void implementLinkedListInPythonBtn() {
+		implementLinkedListInPythonBtn.click();
+	}
+
+	public void traversalBtn() {
+		traversalBtn.click();
+	}
+
+	public void insertionBtn() {
+		insertionBtn.click();
+	}
+
+	public void DeletionBtn() {
+		deletionBtn.click();
+	}
+
+	public void tryHereBtn() {
+		tryHereBtn.click();
+
+	}
+
+	public void nameinput() {
+		WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		webDriverWait.until(ExpectedConditions.visibilityOf(form));
+		form.click();
+		editorInput.sendKeys(ExcelSheetReader.pythonCodeData(2).get(0));
+
+	}
+
+	public void syntaxinput() {
+		WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		webDriverWait.until(ExpectedConditions.visibilityOf(form));
+		form.click();
+		editorInput.sendKeys(ExcelSheetReader.pythonCodeData(3).get(0));
+
+	}
+
+	public void validinput() {
+		WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		webDriverWait.until(ExpectedConditions.visibilityOf(form));
+		form.click();
+		editorInput.sendKeys(ExcelSheetReader.pythonCodeData(1).get(0));
+
+	}
+
+	public void runBtn() {
+		runBtn.click();
+
+	}
+
+	public void practiceQuestionsBtn() {
 		practiceQuestionsBtn.click();
 	}
 
@@ -127,32 +147,42 @@ public class LinkedListPF extends BaseClass {
 	}
 
 	public String handleAlert1() {
-		return handleAlert();
+
+		String handleAlert1 = handleAlert();
+		return handleAlert1;
 
 	}
 
 	public String nameHandleAlert() {
-		return handleAlert();
+
+		String nameHandleAlert = handleAlert();
+		return nameHandleAlert;
 
 	}
 
 	public String syntaxHandleAlert() {
-		return handleAlert();
 
+		String syntaxHandleAlert = handleAlert();
+		return syntaxHandleAlert;
 	}
 
 	public String expectedNameErrorResult() {
-		return ExcelSheetReader.pythonCodeData(2).get(1);
+
+		String expectedNameErrorResult = ExcelSheetReader.pythonCodeData(2).get(1);
+		return expectedNameErrorResult;
 
 	}
 
 	public String expectedSyntaxErrorResult() {
-		return ExcelSheetReader.pythonCodeData(3).get(1);
 
+		String expectedSyntaxErrorResult = ExcelSheetReader.pythonCodeData(3).get(1);
+		return expectedSyntaxErrorResult;
 	}
-	
-	public String expectederrorResult() {
-		return ExcelSheetReader.pythonCodeData(4).get(1);
+
+	public String expectedErrorResult() {
+
+		String expectederrorResult = ExcelSheetReader.pythonCodeData(4).get(1);
+		return expectederrorResult;
 
 	}
 
@@ -204,12 +234,12 @@ public class LinkedListPF extends BaseClass {
 		return ExcelSheetReader.expectedUrl(26).get(0);
 
 	}
-	
+
 	public String expectedUrl27() {
 		return ExcelSheetReader.expectedUrl(27).get(0);
 
 	}
-	
+
 	public String expectedUrl28() {
 		return ExcelSheetReader.expectedUrl(28).get(0);
 
